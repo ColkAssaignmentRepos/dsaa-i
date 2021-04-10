84df6737a1be84ecfec9971b6a97937bb5abba83
Colk-tech <colk.tech@gmail.com>
2021-04-11 00:05:59 +0900

    01: add: Implemented sort of array of int in exer3-1.c

--- src/01/exer3-1.c
+++ src/01/exer3-1.c
@@ -5,38 +5,75 @@
 #include <string.h>
 #define NUMBERS_LENGTH 3
 
-int ask_number(int *target_ptr, char *variable_name);
+int ask_number(int *target_ptr, char variable_name);
+int is_sorted(int target_array[], int array_length);
+int sort_int_array(int target_array[], int *sorted_array_ptr, int array_length);
+int how_many_equal_numbers_in(int target_array[], int array_length);
+
+typedef entry struct {
+    int value,
+    int count
+};
 
 
 int main() {
-    int a, b, c;
+    int numbers[NUMBERS_LENGTH];
+    int sorted[NUMBERS_LENGTH];
+    char variable_names[NUMBERS_LENGTH] = "abc";
     int number_of_equal_numbers;
-    char variable_names[] = "abc";
 
-    ask_number(&a, &variable_names[0]);
-    ask_number(&b, &variable_names[1]);
-    ask_number(&c, &variable_names[2]);
+    for (int i = 0; i < NUMBERS_LENGTH; i++) {
+        ask_number(&numbers[i], variable_names[i]);
+    }
 
-    int numbers[] = {a, b, c};
+    sort_int_array(numbers, sorted, NUMBERS_LENGTH);
 
     return 0;
 }
 
 
-int ask_number(int *target_ptr, char *variable_name) {
-    printf("Please input number to calculate average %c: ", variable_name[0]);
+int ask_number(int *target_ptr, char variable_name) {
+    fflush(stdin);
+
+    printf("Please input number to calculate how many is there equal numbers %c: ", variable_name);
     scanf("%d", target_ptr);
 
     return 0;
 }
 
-int how_many_equal_numbers(int *target) {
-    int result = 0;
-    int length = NUMBERS_LENGTH;
 
-    if (target[0] == target[1] || target[1] == target[2] || target[0] == target[2]) {
-        result += 1;
+int is_sorted(int target_array[], int array_length) {
+    for (int i = 0; i < array_length - 1; i++) {
+        if (target_array[i] > target_array[i+1]) {
+            return 0;
+        }
     }
 
-    return result;
+    return 1;
+}
+
+
+int sort_int_array(int target_array[], int *sorted_array_ptr, int array_length) {
+    int temp;
+
+    memcpy(sorted_array_ptr, target_array, sizeof(int) * NUMBERS_LENGTH);
+
+    while ( !is_sorted(sorted_array_ptr, array_length) ) {
+        for (int i = 0; i < array_length - 1; i++) {
+
+            for (int i = 0; i < array_length; i++) {
+                printf("%d\n", sorted_array_ptr[i]);
+            }
+
+            if (sorted_array_ptr[i] < sorted_array_ptr[i+1]) {
+                temp = sorted_array_ptr[i];
+
+                sorted_array_ptr[i] = sorted_array_ptr[i+1];
+                sorted_array_ptr[i+1] = temp;
+            }
+
+        }
+    }
+
+    return 0;
 }
