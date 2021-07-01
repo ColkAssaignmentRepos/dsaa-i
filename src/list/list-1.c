6c69162891fe03b2c97c8e4e03f4f9f6dfcfc5a9
Colk-tech <colk.tech@gmail.com>
2021-07-01 09:45:13 +0900

    [add] Copy "list-1.c" from the material on Google Classroom

---
+++ src/list/list-1.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _person {
+    char name[50];
+    char phone[50];
+    struct _person *next;
+} person;
+
+
+int main(void) {
+    int s, i;
+    person *head, *p, *tmp;
+
+    printf("How many people?\n");
+    scanf("%d", &s);
+    printf("\n");
+
+    head = NULL;
+    for (i = 1; i <= s; i++) {
+        p = (person *) malloc(sizeof(person));
+        if (p == NULL) {
+            printf("can't get a memory space\n");
+            return 1;
+        }
+
+        printf("Input name : ");
+        scanf("%s", p->name);
+
+        printf("Input phone number : ");
+        scanf("%s", p->phone);
+
+        // TODO: FILL OUT HERE
+        // p->next = __(ア)__;
+        // head = __(イ)__;
+
+        printf("\n");
+
+        /* 後から入力した順に出力 */
+        p = head;
+        while (p != NULL) {
+            printf("name : %s\t phone number : %s\n", p->name, p->phone);
+            p = p->next;
+        }
+        printf("\n");
+
+        /* 後から入力した順に解放 */
+        p = head;
+        while (p != NULL) {
+            tmp = p->next;
+            free(p);
+            p = tmp;
+        }
+
+        return 0;
+    }
+}
